772251394034d94d523a7d6d4e5e6bd87b90f17e
Gustavo Alves Vieira <62160839+BiroGates@users.noreply.github.com>
2022-04-02 17:18:37 -0300

    Update Binary_Search_v2.js

--- Binary_Search_v2.js
+++ Binary_Search_v2.js
@@ -16,22 +16,3 @@ function binarySearch(lis, ele, com, fim){
     if(ele > lis[mid])return binarySearch(lis, ele, mid, fim);
     if(ele < lis[mid])return binarySearch(lis, ele, com, mid); 
 }
-
-
-function getValue(){
-    let v = readLine('Digite um valor: ');
-    return v;
-}
-
-function main(){
-    try{
-        let lista = [3,4,5,6,7,8,9,10,12]
-        let x = getValue();
-        console.log(binarySearch(lista, x, 0, lista.length - 1));
-    }
-    catch(err){
-        console.log(err.message);
-        return false;
-    }
-}
-main();
